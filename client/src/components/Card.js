25d72a886348a41e1d47371f3d8d8be41e9b5745
Bob Stauss <robertrstauss@gmail.com>
2021-05-06 19:10:17 -0400

    basic functionality completed

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,6 +1,34 @@
+import {useEffect, useState} from 'react'
+
+
+
 function Card(props) {
+  const [pokeData, setPokeData] = useState(null)
+
+  useEffect(() => {
+    if(!pokeData || props.name !== pokeData.species.name) {
+      fetch(props.url)
+      .then((res) => res.json())
+      .then(pokeObj => {
+        setPokeData(pokeObj)
+      })
+    }
+  })
+
+
   return(
-    <div></div>
+    <div className="card">
+      {pokeData && (
+      <div>
+        <h3>{props.name}</h3>
+        <img src={pokeData.sprites.front_shiny} alt="pokemon image"/>
+        {pokeData.types.map(slot => {
+          console.log(slot.type)
+          return <p>{slot.type.name}</p>
+        })}
+      </div>
+      )}
+    </div>
   )
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,41 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
 
 function Home(props) {
+  const [pokeList, setPokeList] = useState(null);
 
-  const baseURL = "https://pokeapi.co/api/v2/pokemon"
-  
-  return(
-    <div></div>
-  )
+  const baseURL = "https://pokeapi.co/api/v2/pokemon";
+
+  useEffect(() => {
+    if (!pokeList) {
+      fetch(baseURL)
+        .then((res) => res.json())
+        .then((jsonObj) => {
+          setPokeList(jsonObj);
+        });
+    } else if (
+      props.offset &&
+      props.offset ===
+        parseInt(pokeList.next.split("?")[1].split("&")[0].split("=")[1]) || ((props.offset + 40) === parseInt(pokeList.next.split("?")[1].split("&")[0].split("=")[1]))
+    ) {
+      fetch(baseURL + `?offset=${props.offset}`)
+        .then((res) => res.json())
+        .then((jsonObj) => {
+          setPokeList(jsonObj);
+        });
+    }
+  });
+
+  console.log(pokeList);
+
+  return (
+    <div id="gallery">
+      {pokeList &&
+        pokeList.results.map((pokemon) => {
+          return <Card name={pokemon.name} url={pokemon.url} />;
+        })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
